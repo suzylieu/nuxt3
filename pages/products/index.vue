10c69a3ad2c0e5b07f0c14586314e65c0b8a2d3a
suzylieu <suzy_lieu@worldvision.org.tw>
2023-08-15 01:40:26 +0800

    Fetching Data

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,6 +1,11 @@
 <template>
     <div>
         <h2>Products</h2>
+        <div class="grid grid-cols-4 gap-5">
+            <div v-for="p in products">
+                <NuxtLink :to="`/products/${p.id}`">{{ p.title }}</NuxtLink>
+            </div>
+        </div>
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse facere odit sed placeat. Eveniet, dolorum quae magnam officiis eius tempore?</p>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, nostrum!</p>
     </div>
@@ -10,6 +15,9 @@
     definePageMeta({
         layout: 'products'
     })
+
+    // fetch the products
+    const { data: products } = await useFetch('https://fakestoreapi.com/products')
 </script>
 
 <style scoped>
